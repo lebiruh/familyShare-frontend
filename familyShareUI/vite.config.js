bc81800af815a3d7310db3df128a410bc2a97da0
Biruh <lebiruh@yahoo.com>
2024-12-06 07:22:42 +0300

    changed port

--- familyShareUI/vite.config.js
+++ familyShareUI/vite.config.js
@@ -6,14 +6,15 @@ export default defineConfig({
    
   plugins: [react()],
   server: {
-    // proxy: {
-    //   "/api": {
-    //     target: "http://ec2-18-217-122-189.us-east-2.compute.amazonaws.com:5000",
-    //     // target: "http://localhost:5000",
-    //     changeOrigin: true
-    //   }
-    // },
     host: true,
-    port: 80
+    port: 80,
+    proxy: {
+      "/api": {
+        target: "http://ec2-18-217-122-189.us-east-2.compute.amazonaws.com:5000",
+        // target: "http://localhost:5000",
+        changeOrigin: true
+      }
+    },
+    
   }
 })
